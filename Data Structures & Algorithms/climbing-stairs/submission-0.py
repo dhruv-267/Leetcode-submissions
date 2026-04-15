3b4b57b4026e35689d891a565dd0413edeb9e939
dhruv-267 <77952128+dhruv-267@users.noreply.github.com>
2026-04-15 15:17:50 +0530

    Update: climbing-stairs - submission-0

--- Data Structures & Algorithms/climbing-stairs/submission-0.py
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -1,8 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        i , j  = 1 , 1   #n-1th place ans , nth place ans 
+        i , j = 1,1
+
         for _ in range(n-1):
             temp = i
-            i = i+j
+            i = i + j
             j = temp
-        return i
+        return i 
