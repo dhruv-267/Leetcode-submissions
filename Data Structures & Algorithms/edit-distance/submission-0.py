ba1c3038c95c600a46be19912f9615a04d11680e
dhruv-267 <77952128+dhruv-267@users.noreply.github.com>
2026-04-16 23:14:14 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+
+        @lru_cache(maxsize=None)
+        def dist(i,j):
+            if i==n and j == m:
+                return 0
+            if i==n:
+                return m - j
+            if j==m:
+                return n - i
+            
+            if word1[i]==word2[j]:
+                return dist(i+1,j+1)
+            else:
+                return min(1 + dist(i,j+1), 1 + dist(i+1,j),1 + dist(i+1,j+1))
+            
+        res = dist(0,0)
+        dist.cache_clear()
+        return res
