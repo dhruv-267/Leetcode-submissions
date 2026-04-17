dcc3be21f8e0c1ef1b6df6e44e0452bc409337ac
dhruv-267 <77952128+dhruv-267@users.noreply.github.com>
2026-04-17 18:47:21 +0530

    Add: unboundedKnapsack - submission-0

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-0.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+        n = len(profit)
+        @lru_cache(maxsize = None)
+        def knp(i,capacity):
+            if i == n:
+                return 0
+            
+            maxprofit = knp(i+1,capacity)
+
+            if weight[i]<=capacity:
+                p = profit[i] + knp(i,capacity-weight[i])
+                maxprofit = max(maxprofit,p)
+            
+            return maxprofit
+        res =  knp(0,capacity)
+        knp.cache_clear()
+        return res
