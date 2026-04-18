4ee1fb6cefc2166230a6f5b68c173e7f8c1febac
dhruv-267 <77952128+dhruv-267@users.noreply.github.com>
2026-04-18 17:21:23 +0530

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        l ,r = 0,0
+        res , farthest = 0,0
+        while r < len(nums)-1:
+            for i in range(l,r+1):
+                farthest = max(farthest,i+nums[i])
+            l = r+1
+            r = farthest
+            res+=1
+        return res
+
+            
+
