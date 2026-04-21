a68a46d335ef9a80b2d7802ac8af7cc2766a53d8
dhruv-267 <77952128+dhruv-267@users.noreply.github.com>
2026-04-21 22:42:41 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def sos(n):
+            output = 0
+            while n:
+                div , mod = divmod(n,10)
+                n = div
+                output += mod*mod
+            return output
+        '''
+        # T : O(logn) S: 0(logn)
+        visited = set()
+        while n not in visited:
+            visited.add(n)
+            n = sos(n)
+        return True if n ==1 else False
+        '''
+
+        slow,fast = n,sos(n)
+        while slow!=fast:
+            fast = sos(fast)
+            fast = sos(fast)
+            slow = sos(slow)
+        return True if fast == 1 else False
+        
